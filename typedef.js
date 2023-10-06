6c7dc85135dd7b8347d984274e72296663230744
harkanni <harkanzyempire001@gmail.com>
2023-10-06 17:55:06 +0100

    started graphql server

---
+++ typedef.js
@@ -0,0 +1,38 @@
+
+const messages = [];
+
+export const typeDefs = `#graphql
+   type Message {
+      id: ID!
+      user: String!
+      content: String!  
+   }
+
+   type Query {
+      messages: [Message!]
+   }
+
+   type Mutation {
+      addMessage(user: String!, content: String!): Message
+   }
+`;
+
+export const resolvers = {
+  Query: {
+    messages() {
+      return messages;
+    }
+  },
+
+  Mutation: {
+   addMessage(parent, args) {
+      const id = messages.length;
+      messages.push({
+         id,
+         ...args
+      })
+      return messages[id];
+   }
+  }
+};
+
