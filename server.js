4cf1a032525a9e275e89c147180ecb7f0d867db3
harkanni <harkanzyempire001@gmail.com>
2023-10-07 15:58:44 +0100

    gql_client subscription

--- server.js
+++ server.js
@@ -1,17 +1,70 @@
 import { ApolloServer } from '@apollo/server';
-
-import { startStandaloneServer } from '@apollo/server/standalone';
-
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import { typeDefs, resolvers } from './typedef.js';
 
+import { makeExecutableSchema } from '@graphql-tools/schema'
+import { WebSocketServer  } from 'ws'
+import { useServer } from 'graphql-ws/lib/use/ws'
+
+
+const app = express();
+const httpServer = http.createServer(app)
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers
-});
+   schema,
+   plugins: [
+      // PROPER SHUTDOWN FOR THE HTTP SERVER
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      
+      // PROPER SHUTDOWN FOR THE WEBSOCKET SERVER
+      {
+         async serverWillStart() {
+            return {
+               async drainServer() {
+                  await serverCleanup.dispose();
+               }
+            }
+         }
+      }
+   ],      
+ });
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 }
-});
+ const wsServer = new WebSocketServer({
+   server: httpServer,
+   path: '/graphql',
+   // path: '/subscriptions',
+ })
 
-console.log(`🚀  Server ready at: ${url}`);
+ const serverCleanup = useServer({ schema }, wsServer);
+
+
+ await server.start();
+
+ app.use('/graphql', cors(), bodyParser.json(), expressMiddleware(server, {
+   context: async ({ req }) => ({ token: req.headers.token }),
+ }));
+
+ await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+console.log(`🚀 Server ready at http://localhost:4000/`);
+
+
+
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers
+// });
+
+// const { url } = await startStandaloneServer(server, {
+//   listen: { port: 4000 }
+// });
+
+// console.log(`🚀  Server ready at: ${url}`);
--- typedef.js
+++ typedef.js
@@ -1,4 +1,47 @@
+// import { ApolloServer } from '@apollo/server';
+// import { expressMiddleware } from '@apollo/server/express4';
+// import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+// import express from 'express';
+// import http from 'http';
+// import cors from 'cors';
+// import bodyParser from 'body-parser';
+// import { json } from 'body-parser';
+
+// import { makeExecutableSchema } from '@graphql-tools/schema'
+// import { WebSocketServer  } from 'ws'
+// import { useServer } from 'graphql-ws/lib/use/ws'
+
+
+// const app = express();
+
+// const httpServer = http.createServer(app)
+
+// const server = new ApolloServer({
+//    typeDefs, 
+//    resolvers, 
+//    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+//  });
+
+//  await server.start();
+
+//  app.use('/graphql', cors(), json(), expressMiddleware(server, {
+//    context: async ({ req }) => ({ token: req.headers.token }),
+//  }));
+
+//  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+// console.log(`🚀 Server ready at http://localhost:4000/`);
+
+
+
+
+
+import { PubSub } from 'graphql-subscriptions';
+const pubsub = new PubSub();
+
 const messages = [];
+const subscribers = [];
+const onMessagesUpdate = (fn) => subscribers.push(fn); //
 
 export const typeDefs = `#graphql
    type Message {
@@ -16,7 +59,7 @@ export const typeDefs = `#graphql
    }
 
    type Subscription {
-      postCreated: Post
+      messages: [Message!]
    }
 `;
 
@@ -34,7 +77,13 @@ export const resolvers = {
         id,
         ...args
       });
+      pubsub.publish('NEW_MESSAGE_CREATED', { messages: messages });
       return messages[id];
     }
+  },
+  Subscription: {
+   messages: {
+      subscribe: () => pubsub.asyncIterator(['NEW_MESSAGE_CREATED'])
+   }
   }
 };
